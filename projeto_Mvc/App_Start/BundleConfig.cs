717804e3702e2110447c044804e874e9b54a8ef2
Aluno <Aluno@Digital.net>
2018-04-16 16:22:12 -0300

    primeiro commit

---
+++ projeto_Mvc/App_Start/BundleConfig.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace projeto_Mvc
+{
+    public class BundleConfig
+    {
+        // Para obter mais informações sobre o agrupamento, visite https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/admin-lte/js").Include(
+             "~/admin-lte/js/app.js",
+             "~/admin-lte/plugins/fastclick/fastclick.js",
+              "~/admin-lte/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.all.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use a versão em desenvolvimento do Modernizr para desenvolver e aprender. Em seguida, quando estiver
+            // pronto para a produção, utilize a ferramenta de build em https://modernizr.com para escolher somente os testes que precisa.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/respond.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/site.css",
+                         "~/admin-lte/css/AdminLTE.css",
+                      "~/admin-lte/css/skins/skin-blue.css"));
+        }
+    }
+}
---
+++ projeto_Mvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="projeto_Mvc.MvcApplication" Language="C#" %>
